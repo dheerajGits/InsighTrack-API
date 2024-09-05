b20da0d9ba4911c0ab9bfab45c14b02d23c3d5f1
Dheeraj202 <dheerajsadan2002@gmail.com>
2024-09-05 18:00:05 +0530

    add eslint config files and created app and server.ts

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+import bodyParser from "body-parser";
+import express from "express";
+import http from "http";
+import cors from "cors";
+
+class App {
+  public app: express.Application;
+  public server: http.Server;
+  public port = 3030;
+
+  constructor(routes: any) {
+    this.app = express();
+    this.server = http.createServer(this.app);
+    this.initializeMiddlewares(); // initialize the middlewares when instance is created
+    this.initializeRoutes(routes);
+  }
+  public listen() {
+    this.server.listen(this.port, () => {
+      console.log("-----Server listening on port ", this.port, "----------");
+    });
+  }
+
+  public initializeMiddlewares() {
+    this.app.use(bodyParser.json({ limit: "50mb" }));
+    this.app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" })); // set limit to 50mb
+    this.app.use(cors());
+  }
+  public initializeRoutes(Routes: any) {
+    Routes.map((Route: any) => {
+      this.app.use("/", Route.router);
+    });
+  }
+}
+export default App;
---
+++ src/server.ts
@@ -0,0 +1,4 @@
+import App from "app";
+
+let app = new App([]);
+app.listen();
